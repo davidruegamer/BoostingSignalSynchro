b0085cfd3587f8f808345b7b1d99bd8655956799
David Rügamer <david.ruegamer@stat.uni-muenchen.de>
2016-06-14 08:05:32 +0200

    Add files via upload

---
+++ simulations/0_libs_funs.R
@@ -0,0 +1,11 @@
+### libraries and sourcing
+
+# install.packages("FDboost", repos="http://R-Forge.R-project.org")
+library("parallel")
+library("FDboost")
+library("splines")        
+
+source("../functions/ranHist.R")
+source("../functions/createRandomRespFolds.R")
+source("../functions/dataGenProc.R")
+source("../functions/bootMod.R")
